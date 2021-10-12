f788331577255431b5309fbd39bea285dce334cd
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2021-10-12 18:02:24 +0300

    aymn made new calc

--- src/MAINAYMN.kt
+++ src/MAINAYMN.kt
@@ -1,11 +1,27 @@
 fun main(){
-    var PlayerName="zamel"
-    var playerHealth=100
-    if(playerHealth==100){
-        println(PlayerName+" is great")
+  var   x= readLine()!!
+    var mark= readLine()!!
+    var y= readLine()!!
+    if (mark=="+"){
+       var r=x.toInt()+y.toInt()
+        println(r)
     }
-    else{
-        println(PlayerName+" is not great")
+ else if (mark=="-"){
+     var r = x.toInt()- y.toInt()
+        println(r)
+    }
+else if (mark=="*"){
+    var r=x.toInt()*y.toInt()
+    println(r)
+    }
+    else if (mark=="/") {
+        var r = x.toInt() / y.toInt()
+        println(r)
+    }else if (mark=="%"){
+        var r=x.toInt()%y.toInt()
+        println(r)}
+
     }
 
-}
+
+
