d7b90a997ea63779555a4c99c8ee26d089a90f6f
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2021-10-12 18:31:08 +0300

    aymn made new salary calc

--- src/MAINAYMN.kt
+++ src/MAINAYMN.kt
@@ -1,27 +1,20 @@
 fun main(){
-  var   x= readLine()!!
-    var mark= readLine()!!
-    var y= readLine()!!
-    if (mark=="+"){
-       var r=x.toInt()+y.toInt()
-        println(r)
-    }
- else if (mark=="-"){
-     var r = x.toInt()- y.toInt()
-        println(r)
-    }
-else if (mark=="*"){
-    var r=x.toInt()*y.toInt()
-    println(r)
-    }
-    else if (mark=="/") {
-        var r = x.toInt() / y.toInt()
-        println(r)
-    }else if (mark=="%"){
-        var r=x.toInt()%y.toInt()
+    println(" put your salary")
+    var salary= readLine()!!
+    println("put you preformnce")
+    var preformnce = readLine()!!
+    if (preformnce.toInt()==1){
+       var r=salary.toInt()+(salary.toInt()*0.06)
         println(r)}
-
+    else if (preformnce.toInt()==2){
+        var r=salary.toInt()+(salary.toInt()*0.04)
+        println(r)
     }
-
-
-
+    else if (preformnce.toInt()==3){
+        var r=salary.toInt()+(salary.toInt()*0.015)
+        println(r)
+    }
+    else{
+        println("error")
+    }
+}
