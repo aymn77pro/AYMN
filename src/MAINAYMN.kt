093e3bd6984356da3ea7621b338c3d16c49819ca
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2021-10-12 15:37:23 +0300

    player health

--- src/MAINAYMN.kt
+++ src/MAINAYMN.kt
@@ -1,3 +1,11 @@
 fun aymn(){
-    
+    var PlayerName="zamel"
+    var playerHealth=100
+    if(playerHealth==100){
+        println(PlayerName+" is great")
+    }
+    else{
+        println(PlayerName+" is not great")
+    }
+
 }
