1d378f07bf4aa402640cd8e4b346ae0462edc2d9
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2021-10-13 17:41:54 +0300

    player data

--- src/MAINAYMN.kt
+++ src/MAINAYMN.kt
@@ -1,10 +1,26 @@
 
-fun main(args: Array<String>) {
-    var num1 = readLine()!!.toFloat()
-    var num2 = readLine()!!.toFloat()
-    var num3 = readLine()!!.toFloat()
-    var smallest=getSmallest(num1,num2,num3)
-    var biggest=gitBigger(num1,num2,num3)
-    var mid=gitmid(num1,num2,num3)
-
-    println("$smallest,$mid,$biggest") }
+fun main() {
+    val name = "Madrigal"
+    var healthPoints = 89
+    val isBlessed = true
+    val isImmortal = false
+    println("$name your heath is ")
+    if (isBlessed && healthPoints>50||isImmortal){
+        println("green")
+    }
+    else{
+        println("none")
+    }
+    if (isBlessed){
+        println("$name is blessed")}
+    else{
+        println("$name is not blessed")}
+    val karma = (Math.pow(Math.random(),(110 - healthPoints) / 100.0) * 20 ).toInt()
+    println("$name  your karma is")
+    when(karma){
+       in 0..5 -> println("red")
+        in 6..10 -> println("orange")
+        in 11..15-> println("purle")
+        in 16..20-> println("green")
+    }
+}
