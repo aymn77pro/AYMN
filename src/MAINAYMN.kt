042c2741faeffde02aab236d44eb7cddad3b120e
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2021-10-13 16:37:04 +0300

    big small mid new shortcut

--- src/MAINAYMN.kt
+++ src/MAINAYMN.kt
@@ -1,14 +1,13 @@
-fun main(args: Array<String>) {
-    var num1 = readLine()!!.toFloat()
-    var num2 = readLine()!!.toFloat()
-    var num3 = readLine()!!.toFloat()
-    var smallest=num1
+fun getSmallest(num1:Float,num2:Float,num3: Float):Float{ var smallest=num1
     if (smallest>=num2){
         smallest=num2
     }
     if (smallest>=num3){
         smallest=num3
     }
+    return smallest
+}
+fun gitBigger(num1:Float,num2:Float,num3:Float):Float{
     var biggest=num1
     if (biggest<=num2){
         biggest=num2
@@ -16,15 +15,28 @@ fun main(args: Array<String>) {
     if (biggest<=num3){
         biggest=num3
     }
+    return biggest
+}
+fun gitmid(num1: Float,num2: Float,num3:Float): Float{
+    var smallest=getSmallest(num1,num2,num3)
+    var biggest=gitBigger(num1,num2,num3)
     var mid=num1
-    if (num1>num2 && num1<num3||num1<num2&&num1>num3){
+    if (num1>smallest&&num1<biggest){
         mid =  num1
     }
-    if ((num2>num1 && num2<num3||num2<num1&&num2>num3))
+    else if (num2>smallest&&num2<biggest)
     {mid=num2}
-    if(num3>num2 && num3<num1||num3<num2&&num3>num1){
+    else{
         mid=num3
     }
-
-    println("$smallest,$mid,$biggest")
+    return mid
 }
+fun main(args: Array<String>) {
+    var num1 = readLine()!!.toFloat()
+    var num2 = readLine()!!.toFloat()
+    var num3 = readLine()!!.toFloat()
+    var smallest=getSmallest(num1,num2,num3)
+    var biggest=gitBigger(num1,num2,num3)
+    var mid=gitmid(num1,num2,num3)
+
+    println("$smallest,$mid,$biggest") }
