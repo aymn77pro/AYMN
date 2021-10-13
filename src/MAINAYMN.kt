24e0cb382070c599423729ee0ee9d2a342e4a0e4
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2021-10-13 17:07:40 +0300

    big small mid new shortcut 2

--- src/MAINAYMN.kt
+++ src/MAINAYMN.kt
@@ -1,36 +1,4 @@
-fun getSmallest(num1:Float,num2:Float,num3: Float):Float{ var smallest=num1
-    if (smallest>=num2){
-        smallest=num2
-    }
-    if (smallest>=num3){
-        smallest=num3
-    }
-    return smallest
-}
-fun gitBigger(num1:Float,num2:Float,num3:Float):Float{
-    var biggest=num1
-    if (biggest<=num2){
-        biggest=num2
-    }
-    if (biggest<=num3){
-        biggest=num3
-    }
-    return biggest
-}
-fun gitmid(num1: Float,num2: Float,num3:Float): Float{
-    var smallest=getSmallest(num1,num2,num3)
-    var biggest=gitBigger(num1,num2,num3)
-    var mid=num1
-    if (num1>smallest&&num1<biggest){
-        mid =  num1
-    }
-    else if (num2>smallest&&num2<biggest)
-    {mid=num2}
-    else{
-        mid=num3
-    }
-    return mid
-}
+
 fun main(args: Array<String>) {
     var num1 = readLine()!!.toFloat()
     var num2 = readLine()!!.toFloat()
--- src/testinge.kt
+++
@@ -1,7 +0,0 @@
- fun main() {
-        val name = "Madrigal"
-        var healthPoints = 89
-        val isBlessed = true
-        val isImmortal = false
-}
-
